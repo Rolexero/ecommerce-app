5ce67bb2b36926606435123af48cf3fbfc72e311
ph-dev-labs <nwosuchimezie95@gmail.com>
2022-06-30 13:38:55 +0200

    added strapi headless cms to the application and added little styles to the pages

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import HomePage from "./pages/Homepage.component";
 import ShopPage from "./pages/Shop-page.component";
 import AboutPage from "./pages/About-page.component";
 import ContactPage from "./pages/Contact-page.component";
+import Navbar from "./Components/Navbar/Navbar"
+import Footer from "./Components/Footer/Footer.component";
+
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/shop" element={<ShopPage />} />
       </Routes>
+      <Footer/>
     </>
   );
 }
--- src/pages/Homepage.component.jsx
+++ src/pages/Homepage.component.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import Navbar from "../Components/Navbar/Navbar";
-import Footer from "../Components/Footer/Footer.component";
-
 const HomePage = () => {
     return (
         <div className="homepage">
-            <Navbar />
-            <Footer />
+           
         </div>
     )
 }
